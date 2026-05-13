13abde1fe2177342f253a2b355b7cdf17af1a9f7
Ayush-Chouhan1 <ayushraochouhan@gmail.com>
2026-05-13 16:59:56 +0530

    [Ayush]: added alert service

---
+++ microservicearchitecture/alert-service/src/test/java/com/stockpro/alertservice/AlertServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.stockpro.alertservice;
+
+import org.junit.jupiter.api.Test;
+
+class AlertServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Intentionally empty - full context test requires running infrastructure
+        // Unit tests are in AlertServiceTest.java
+    }
+}
